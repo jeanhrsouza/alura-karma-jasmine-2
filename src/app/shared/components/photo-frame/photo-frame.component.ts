b4e344c3fa76a4149f5fae0d90a8c4a66bf1cea6
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-19 15:53:37 -0300

    Componente photoframe

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFramecomponent {}
+export class PhotoFramecomponent {
+  @Output() public liked: EventEmitter<void> = new EventEmitter();
+  @Input() public description: string = '';
+  @Input() public src: string = '';
+  @Input() public likes: number = 0;
+
+  public like(): void {
+    this.liked.emit();
+  }
+}
